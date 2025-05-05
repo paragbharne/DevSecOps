27b39a1f37920b8ae04969e9af274716ae66e113
Parag Bharne <parag.bharne@globant.com>
2025-05-05 18:03:52 +0530

    fix: Remove error handling from ESLint command and standardize ESLint configuration format

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-{
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
   },
-  "rules": {
+  rules: {
     "no-unused-vars": "warn",
     "no-console": "off"
   }
-}
+};
